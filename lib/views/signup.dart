9d136126539e776b0f4a2c396dde7c053b82b05f
Steven Attipoe <stevenattipoe7@gmail.com>
2022-04-10 23:59:05 +0000

    reverting to dcd9a8f

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,99 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:workwaves/views/home.dart';
-import 'package:form_validator/form_validator.dart';
 
 class SignUp extends StatefulWidget {
-  const SignUp({Key? key}) : super(key: key);
+  const SignUp({ Key? key }) : super(key: key);
 
   @override
   State<SignUp> createState() => _SignUpState();
 }
 
 class _SignUpState extends State<SignUp> {
-  GlobalKey<FormState> _form = GlobalKey<FormState>();
-
-  void _validate() {
-    _form.currentState!.validate();
-  }
-
   @override
   Widget build(BuildContext context) {
-    RegExp exp =
-        RegExp("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@!%.*#?&])[A-Za-z\d@!%*#?&]{8,}");
-    String str =
-        "Must include alphanumeric and symbol and atleast 8 characters";
-
     return Scaffold(
-      body: Form(
-        key: _form,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                validator: ValidationBuilder()
-                    .minLength(5)
-                    .maxLength(50)
-                    .required()
-                    .build(),
-                decoration: InputDecoration(
-                  labelText: 'Name',
-                  helperText: 'Min length: 5, max length: 50',
-                ),
-              ),
-              TextFormField(
-                validator: ValidationBuilder()
-                    .minLength(8)
-                    .maxLength(20)
-                    .regExp(exp, str)
-                    .required()
-                    .build(),
-                /*validator: requiredValidator,*/
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  helperText:
-                      'Min length: 6, max length: 16, Must contain a symbol or number',
-                ),
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-              ),
-              TextFormField(
-                validator: ValidationBuilder()
-                    .email()
-                    .maxLength(50)
-                    .required()
-                    .build(),
-                decoration: InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.text,
-              ),
-              TextFormField(
-                validator: ValidationBuilder().phone().maxLength(10).build(),
-                decoration: InputDecoration(labelText: 'Phone Number'),
-              ),
-              SizedBox(height: 30),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        /* tooltip: 'Next',*/
-        child: Text('Sign-Up'),
-        onPressed: () {
-          _validate();
+          body: SafeArea(
+            child:  Column(
+              children: [
+                Text("SignUp!"),
 
-          Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (context) => Home()));
-        },
-      ),
-      /*ElevatedButton: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context) => Home()));
-          },
-          child: Text('Sign-Up'),
-          style: ElevatedButton.styleFrom(primary: Colors.black)),*/
+              ElevatedButton(
+                onPressed: () {
+                    Navigator.pushReplacement(
+                    context, 
+                    MaterialPageRoute(
+                      builder: (context)=>Home()));
+                },
+                child: Text('Login'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black
+                )
+              )],    
+            )
+          ),
     );
   }
 }
