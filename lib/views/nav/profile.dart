f673a64cf06f5d76545b5d6eef7510eec5dc654c
Implax <juniourclottey@gmail.com>
2022-04-11 18:12:53 +0000

    Update Profile and Review Screen

--- lib/views/nav/profile.dart
+++ lib/views/nav/profile.dart
@@ -1,19 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:workwaves/views/nav/review_screen.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({ Key? key }) : super(key: key);
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final double coverHeight = 280;
+  final double profileHeight = 144;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body: SafeArea(
-            child: const Text("Profile"),
-          ),
+      body: ListView(padding: EdgeInsets.zero, children: <Widget>[
+        buildTop(),
+        buildContent(),
+        SizedBox(height: 20),
+        buildReviews(),
+      ]),
     );
   }
+
+  Widget buildTop() {
+    final top = coverHeight - profileHeight / 2;
+
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+        buildCoverImage(),
+        Positioned(
+          top: top,
+          child: buildProfileImage(),
+        ),
+      ],
+    );
+  }
+
+  Widget buildContent() => Column(
+        children: <Widget>[
+          SizedBox(height: 60),
+          Text(
+            'Jesse Srodah',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            'UX Designer',
+            style: TextStyle(
+              fontSize: 18,
+              color: Color(0xff99879D),
+            ),
+          ),
+          SizedBox(height: 15),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: Column(
+                children: [
+                  Text(
+                    'Description',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'My name is Jesse, I’m a young designer from Ghana. I school at Ashesi University and I am currently in my third year. I offer Computer Science.',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff99879D),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+
+  Widget buildReviews() => Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: Text('64 Reviews'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                            builder: (context) => new ReviewScreen()));
+                  },
+                  child: const Text('View All'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black54,
+                  )),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildCoverImage() => Container(
+        color: Colors.grey,
+        child: Image.network(
+          "https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+          height: coverHeight,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        ),
+      );
+
+  Widget buildProfileImage() => CircleAvatar(
+        radius: profileHeight / 2.5,
+        backgroundColor: Colors.grey.shade800,
+        backgroundImage: NetworkImage(
+          "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
+        ),
+      );
 }
