6f25739c490ba1f8c9332f793bd2ba94c554a3df
Implax <juniourclottey@gmail.com>
2022-04-24 16:52:07 +0000

    Added User Class

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,1 +1,17 @@
-//Email & Password Sign Up
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> userSetup(
+    String email, String fullName, String phone, String role) async {
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String uid = auth.currentUser!.uid.toString();
+  users.add({
+    'uid': uid,
+    'email': email,
+    'fullName': fullName,
+    'phone': phone,
+    'role': role
+  });
+  return;
+}
